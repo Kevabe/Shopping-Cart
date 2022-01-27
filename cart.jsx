68c724949cc5998d15d0ddb47f36f07a6062d5bf
Kevabe <89057457+Kevabe@users.noreply.github.com>
2022-01-26 23:48:08 -0700

    Update cart.jsx

--- cart.jsx
+++ cart.jsx
@@ -1,8 +1,8 @@
 // simulate getting products from DataBase
 const products = [
-  { name: "Apples_:", country: "Italy", cost: 3, instock: 10 },
+  { name: "Apples:", country: "Italy", cost: 3, instock: 10 },
   { name: "Oranges:", country: "Spain", cost: 4, instock: 3 },
-  { name: "Beans__:", country: "USA", cost: 2, instock: 5 },
+  { name: "Beans:", country: "USA", cost: 2, instock: 5 },
   { name: "Cabbage:", country: "USA", cost: 1, instock: 8 },
 ];
 //=========Cart=============
@@ -90,9 +90,9 @@ const Products = (props) => {
   } = ReactBootstrap;
   //  Fetch Data
   const { Fragment, useState, useEffect, useReducer } = React;
-  const [query, setQuery] = useState("http://localhost:1337/api/products");
+  const [query, setQuery] = useState("http://localhost:1337/products");
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
-    "http://localhost:1337/api/products",
+    "http://localhost:1337/products",
     {
       data: [],
     }
@@ -102,114 +102,8 @@ const Products = (props) => {
   const addToCart = (e) => {
     let name = e.target.name;
     let item = items.filter((item) => item.name == name);
+    if (item[0].instock == 0) return;
+    item[0].instock = item[0].instock - 1;
     console.log(`add to Cart ${JSON.stringify(item)}`);
-    setCart([...cart, ...item]);
-    //doFetch(query);
-  };
-  const deleteCartItem = (index) => {
-    let newCart = cart.filter((item, i) => index != i);
-    setCart(newCart);
-  };
-  const photos = ["apple.png", "orange.png", "beans.png", "cabbage.png"];
-
-  let list = items.map((item, index) => {
-    //let n = index + 1049;
-    //let url = "https://picsum.photos/id/" + n + "/50/50";
-
-    return (
-      <li key={index}>
-        <Image src={photos[index % 4]} width={70} roundedCircle></Image>
-        <Button variant="primary" size="large">
-          {item.name}:{item.cost}
-        </Button>
-        <input name={item.name} type="submit" onClick={addToCart}></input>
-      </li>
-    );
-  });
-  let cartList = cart.map((item, index) => {
-    return (
-      <Card key={index}>
-        <Card.Header>
-          <Accordion.Toggle as={Button} variant="link" eventKey={1 + index}>
-            {item.name}
-          </Accordion.Toggle>
-        </Card.Header>
-        <Accordion.Collapse
-          onClick={() => deleteCartItem(index)}
-          eventKey={1 + index}
-        >
-          <Card.Body>
-            $ {item.cost} from {item.country}
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    );
-  });
-
-  let finalList = () => {
-    let total = checkOut();
-    let final = cart.map((item, index) => {
-      return (
-        <div key={index} index={index}>
-          {item.name}
-        </div>
-      );
-    });
-    return { final, total };
-  };
-
-  const checkOut = () => {
-    let costs = cart.map((item) => item.cost);
-    const reducer = (accum, current) => accum + current;
-    let newTotal = costs.reduce(reducer, 0);
-    console.log(`total updated to ${newTotal}`);
-    return newTotal;
-  };
-  // TODO: implement the restockProducts function
-  const restockProducts = (url) => {
-    doFetch(url);
-    let newItems = data.map((item) => {
-      let {name, country, cost, instock };
-      return {name, country, cost, instcok};
-    });
-    setItems([...items, ...newItems]);
-  };
-
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Product List</h1>
-          <ul style={{ listStyleType: "none" }}>{list}</ul>
-        </Col>
-        <Col>
-          <h1>Cart Contents</h1>
-          <Accordion>{cartList}</Accordion>
-        </Col>
-        <Col>
-          <h1>CheckOut </h1>
-          <Button onClick={checkOut}>CheckOut $ {finalList().total}</Button>
-          <div> {finalList().total > 0 && finalList().final} </div>
-        </Col>
-      </Row>
-      <Row>
-        <form
-          onSubmit={(event) => {
-            restockProducts(`http://localhost:1337/${query}`);
-            console.log(`Restock called on ${query}`);
-            event.preventDefault();
-          }}
-        >
-          <input
-            type="text"
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          <button type="submit">ReStock Products</button>
-        </form>
-      </Row>
-    </Container>
-  );
-};
-// ========================================
-ReactDOM.render(<Products />, document.getElementById("root"));
+  
+    setCart([...cart, ...item])
